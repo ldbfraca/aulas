4e7974fd1f86aa3cb430f7a827bf30ba5475d56a
ldbfraca <ldbfranca@gmail.com>
2022-10-21 15:12:06 -0300

    dvs
    vsd

--- react/e8/scripts.js
+++
@@ -1,9 +0,0 @@
-function sorteador(...numeros){
-    console.log(numeros);
-
-    const numeroGerado = Math.floor(Math.random() * numeros.length);
-
-    console.log(numeros[numeroGerado])
-}
-
-sorteador(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
