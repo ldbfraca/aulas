5043659668258ebda5432d0d64b74c7d66727f5c
ldbfraca <ldbfranca@gmail.com>
2022-11-04 17:21:53 -0300

    abedfrb
    abderfbaer

--- python/d2.py
+++ python/d2.py
@@ -1,6 +1,6 @@
 ''' faça um programa que leia 3 valores inteiros e imprima o maior e o menor deles'''
 
-'''a = int(input('diga qual o valor 1: '))
+a = int(input('diga qual o valor 1: '))
 b = int(input('diga qual o valor 2: '))
 c = int(input('diga qual o valor 3: '))
 
@@ -8,5 +8,12 @@ c = int(input('diga qual o valor 3: '))
 if a == b == c:
     print(f'os valores sao iguais: {a}')
 
-#elif a = b & a > c:
-    '''
+elif a > b & a > c:
+    print(f'o valor a: {a} é maior')
+
+elif b > a & b > c:
+    print(f' o valor {b} é maior')
+
+elif c > b & c > a:
+    print(f'c é maior, com {c}')
+
