0ae8b807cca51aed5ab45f4e85822b6ec9372603
ldbfraca <ldbfranca@gmail.com>
2022-11-10 16:02:28 -0300

    fwe
    fwe

---
+++ python/invest2/tela3.py
@@ -0,0 +1,43 @@
+import PySimpleGUI as sg
+import bibliotecas
+layout = [  [sg.T(" ")],
+            [sg.T(" ")],
+            [sg.T("Qual foi o valor?"), sg.InputText('', key='valor')],
+            [sg.T(" ")],
+            [sg.T(" ")],
+            [sg.Button('Calcular'), sg.Cancel()],
+            [sg.Text('', key='valor recebido')],
+          ]
+
+
+janela = sg.Window('LOTAR DINHEIRINHO sem ter reserva', layout, size=(400, 300))
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WIN_CLOSED or eventos == 'Cancel':
+        break
+
+    if eventos == 'Calcular':
+        reais = int(valores['valor'])
+
+
+        if valores['salario'] == True and valores['recebeu fora'] == True and valores['investimento'] == True \
+                or valores['salario'] == True and valores['recebeu fora'] == True \
+                or valores['salario'] == True and valores['investimento'] == True \
+                or valores['recebeu fora'] == True and valores['investimento'] == True:
+            janela['valor recebido'].update('APERTOU DOIS OU MAIS')
+
+
+        elif valores['salario'] == True and valores['recebeu fora'] == False and valores['investimento'] == False:#só salario é verdadeiro
+            janela['valor recebido'].update(distribuicao.recebeusalario(reais))
+
+
+        elif valores['recebeu fora'] == True and valores['salario'] == False and valores['investimento'] == False: #só recebeu fora verdadeiro
+            janela['valor recebido'].update(distribuicao.recebeuporfora(reais))
+
+
+        elif valores['recebeu fora'] == False and valores['salario'] == False and valores['investimento'] == True:
+            janela['valor recebido'].update(distribuicao.investimentos(reais))
+
+        else:
+            janela['valor recebido'].update('APERTE AO MENOS UM PARA CALCULAR')
+janela.close()
