55aa3acabded1efad53f12083c2eaeef9864015e
ldbfraca <ldbfranca@gmail.com>
2022-12-17 10:47:14 -0300

    fefe
    fefe

---
+++ python/sorteio legado/sorteio.py
@@ -0,0 +1,24 @@
+import random
+
+pessoas = [
+ 'Raissa (indicou Guilherme)',
+ 'Bruna (indicou Yanca) (A)',
+ 'Bruna (indicou Elizeth) (A)',
+ 'Simone (indicou Marcelo) (A)',
+ 'Renato (indicou Nayara)',
+ 'Gabi VB (indicou Nayara) (A)',
+ 'Lairton (indicou Ana Claudia)',
+ 'Lairton (indicou Matheus)',
+ 'Lairton (indicou Isabela)',
+ 'Ariane (indicou Carina) (A)',
+ 'Matheus (indicou Vaneide)',
+ 'Lucas (indicou Elizier) (A)',
+ 'Ingred (indicou Cleber)',
+ 'Acassio (indicou Marcelo)',
+ 'Leticia (indicou Juliana)',
+ 'Joyce (indicou Juliana)',
+ 'Joyce (indicou Matheus)',
+ 'Nat Gomes (indicou Jaqueline)' ]
+
+
+print('O ganhador foi: \n\n\n\n' + pessoas [ random.randrange ( len ( pessoas ))])
